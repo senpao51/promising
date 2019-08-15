a99b1864eed494b025775f3e269dd4c6109c8bba
senpao51 <1773552201@qq.com>
2019-08-15 23:14:56 +0800

    修改代码

--- 8.14/8.14/8.14.c
+++ 8.14/8.14/8.14.c
@@ -99,62 +99,62 @@
 //3.编写代码模拟三次密码输入的场景。
 //最多能输入三次密码，密码正确，提示“登录成功”, 密码错误，
 //可以重新输入，最多输入三次。三次均错，则提示退出程序。
-#include<stdio.h>
-#include <string.h>
-int main()
-{
-	char input[20] = {0};
-	int i = 0;
-	printf("请输入密码\n");
-	for (i = 0; i < 3; i++)
-	{
-		scanf("%s", input);
-		if (strcmp("abcdef", input) == 0)
-		{
-			printf("登录成功\n");
-			break;
-		}
-		else
-		{
-			printf("输入错误\n");
-		}
-	}
-	if (i == 3)
-	{
-		printf("三次均输入错误，退出程序\n");
-	}
-	return 0;
-}
+//#include<stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char input[20] = {0};
+//	int i = 0;
+//	printf("请输入密码\n");
+//	for (i = 0; i < 3; i++)
+//	{
+//		scanf("%s", input);
+//		if (strcmp("abcdef", input) == 0)
+//		{
+//			printf("登录成功\n");
+//			break;
+//		}
+//		else
+//		{
+//			printf("输入错误\n");
+//		}
+//	}
+//	if (i == 3)
+//	{
+//		printf("三次均输入错误，退出程序\n");
+//	}
+//	return 0;
+//}
 //4.编写一个程序，可以一直接收键盘字符，
 //如果是小写字符就输出对应的大写字符，
 //如果接收的是大写字符，就输出对应的小写字符，
 //如果是数字不输出。
-#include <stdio.h>
-int main(void)
-{
-	char a = '0';
-	int b = 0;
-	while (1)
-	{
-		scanf("%c", &a);
-		b = (int)a;
-		if (b <= 90 && b >= 65)
-		{
-			b += 32;
-			printf("%c\n", b);
-		}
-		else if (b <= 122 && b >= 97)
-		{
-			b -= 32;
-			; printf("%c\n", b);
-		}
-		else
-		{
-			;
-		}
-	}
-	return 0;
-}
+//#include <stdio.h>
+//int main(void)
+//{
+//	char a = '0';
+//	int b = 0;
+//	while (1)
+//	{
+//		scanf("%c", &a);
+//		b = (int)a;
+//		if (b <= 90 && b >= 65)
+//		{
+//			b += 32;
+//			printf("%c\n", b);
+//		}
+//		else if (b <= 122 && b >= 97)
+//		{
+//			b -= 32;
+//			; printf("%c\n", b);
+//		}
+//		else
+//		{
+//			;
+//		}
+//	}
+//	return 0;
+//}
 #include <stdio.h>
 int find(int arr[], int b, int c)
 {
@@ -180,9 +180,9 @@ int find(int arr[], int b, int c)
 }
 int main()
 {
-	int arr[] = {1,2,3,4,5,6,7,8,9,10};
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int a = find(arr, 9, sz);
+	int arr1[] = {1,2,3,4,5,6,7,8,9,10};
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int a = find(arr1, 9, sz);
 	if (-1 == a)
 	{
 		printf("找不到\n");
@@ -193,58 +193,58 @@ int main()
 	}
 	return 0;
 }
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-void menu()
-{
-	printf("1:玩游戏\n");
-	printf("2:退出\n");
-}
-void game()
-{
-	int sj = rand()%100+1;
-	int num = 0; 
-	while (1)
-	{
-		printf("请输入要猜的数字\n");
-		scanf("%d", &num);
-		if (num > sj)
-		{
-			printf("猜大了\n");
-		}
-		 else if (num < sj)
-		{
-			printf("猜小了\n");
-		}
-		 else
-		{
-			 printf("恭喜你，猜对了!!!!!!!!!\n");
-			 break;
-		}
-	}
-}
-int main()
-{
-	menu();
-	int input = 0;
-	srand((unsigned int)(time(NULL)));
-	printf("请选择\n");
-	do{
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 2:
-			printf("退出游戏\n");
-			break;
-		default:
-			printf("输入错误，请重新输入\n");
-			break;
-		}
-	} 
-	while (1);
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//void menu()
+//{
+//	printf("1:玩游戏\n");
+//	printf("2:退出\n");
+//}
+//void game()
+//{
+//	int sj = rand()%100+1;
+//	int num = 0; 
+//	while (1)
+//	{
+//		printf("请输入要猜的数字\n");
+//		scanf("%d", &num);
+//		if (num > sj)
+//		{
+//			printf("猜大了\n");
+//		}
+//		 else if (num < sj)
+//		{
+//			printf("猜小了\n");
+//		}
+//		 else
+//		{
+//			 printf("恭喜你，猜对了!!!!!!!!!\n");
+//			 break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	menu();
+//	int input = 0;
+//	srand((unsigned int)(time(NULL)));
+//	printf("请选择\n");
+//	do{
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 2:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("输入错误，请重新输入\n");
+//			break;
+//		}
+//	} 
+//	while (1);
+//	return 0;
+//}
