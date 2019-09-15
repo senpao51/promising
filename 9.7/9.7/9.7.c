8089cec22402b2aa67e605dc1dd0ce326241deaf
senpao51 <1773552201@qq.com>
2019-09-15 23:23:14 +0800

    修改代码

--- 9.7/9.7/9.7.c
+++ 9.7/9.7/9.7.c
@@ -33,47 +33,47 @@
 
 
 //要求写一个函数，将字符串中的字符的空格替换成%20。
-//void replace(char* p)
-//{
-//	int blank = 0;
-//	int len1 = 0;
-//	int len2 = 0;
-//	int before = 0;
-//	int after = 0;
-//	while (p[len1] != '\0')
-//	{
-//		if (p[len1] == ' ')
-//		{
-//			blank++;
-//		}
-//		len1++;
-//	}
-//	len2 = len1 + 2 * blank;
-//	before = len1 - 1;
-//	after = len2 - 1;
-//	while (before != 0)
-//	{
-//		if (p[before] == ' ')
-//		{
-//			p[after] = '0';
-//			p[after-1] = '2';
-//			p[after-2] = '%';
-//			after -= 2;
-//		}
-//		else
-//		{
-//			p[after] = p[before];
-//		}
-//		after--;
-//		before--;
-//	}
-//}
-//int main()
-//{
-//	char arr[30] = "abc defgx yz";
-//	replace(arr);
-//	printf("%s\n",arr);
-//	return 0;
-//}
+void replace(char* p)
+{
+	int blank = 0;
+	int len1 = 0;
+	int len2 = 0;
+	int before = 0;
+	int after = 0;
+	while (p[len1] != '\0')
+	{
+		if (p[len1] == ' ')
+		{
+			blank++;
+		}
+		len1++;
+	}
+	len2 = len1 + 2 * blank;
+	before = len1 - 1;
+	after = len2 - 1;
+	while (before != 0)
+	{
+		if (p[before] == ' ')
+		{
+			p[after] = '0';
+			p[after-1] = '2';
+			p[after-2] = '%';
+			after -= 2;
+		}
+		else
+		{
+			p[after] = p[before];
+		}
+		after--;
+		before--;
+	}
+}
+int main()
+{
+	char arr[30] = "abc defgx yz";
+	replace(arr);
+	printf("%s\n",arr);
+	return 0;
+}
 
 
