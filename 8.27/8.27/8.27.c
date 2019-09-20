0545c3e0c8e700fc97eedd26f4357bf9fb33b3b4
senpao51 <1773552201@qq.com>
2019-09-20 23:45:38 +0800

    上传代码

--- 8.27/8.27/8.27.c
+++
@@ -1,7 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main()
-{
-	printf("司法局可能就");
-	return 0;
-}
--- 9.19/9.19/9.19.c
+++
@@ -1,6 +0,0 @@
-#define _CRT_SECURE_NO_WARNING
-#include <stdio.h>
-int main()
-{
-	return 0;
-}
