0c8b92f6ee6dc32a09c88cd1b9e4ac77af0f4fdc
senpao51 <1773552201@qq.com>
2019-09-22 22:26:48 +0800

    上传代码

---
+++ 9.22/9.22/9.22.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <assert.h>
+//int main()
+//{
+//	int arr[] = { 2, 7, 11, 14 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = printf(arr,sz,9,);
+//	if (-1 == ret)
+//	{
+//		printf("’“≤ªµΩ");
+//	}
+//
+//	return 0;
+//}
+
+
+char* my_strstr(const char*p1, const char*p2)
+{
+	assert(p1 != NULL);
+	assert(p2 != NULL);
+	const char* s1 = p1;
+	const char* s2 = p2;
+	const char* start = s1;
+	while (*start)
+	{
+		s1 = start;
+		while (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+		{
+			return start;
+		}
+		start++;
+		s2=p2;
+	}
+	return NULL;
+}
+int main()
+{
+	char arr[] = "senpaolove";
+	char*ret = my_strstr(arr, "paol");
+	printf("%s\n",ret);
+	return 0;
+}
