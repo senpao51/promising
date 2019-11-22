4bc82a859a8d5d7be43dcac4eabf407cb365d99e
senpao51 <1773552201@qq.com>
2019-11-22 21:53:17 +0800

    引用

---
+++ cpppractice/cpppractice/test.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+//int main()
+//{
+//	int a;
+//	char b;
+//	cin >> a >> b;
+//	cout << "a=" << a<<"  "<<"b="<<b<<endl;
+//}
+//void TestFunc(int a = 10)
+//{
+//	cout << "void TestFunc(int10)" << endl;
+//}
+//void TestFunc(int a)
+//{
+//	cout << "void TestFunc(int20)" << endl;
+//}
+//int main()
+//{
+//	TestFunc(10);
+//}
+//int main()
+//{
+//	int a = 10;
+//	int &ra = a;
+//	cout << &ra << endl;
+//	cout << &a << endl;
+//	int *pa = &ra;
+//	int*& pra = pa;
+//	int&rra = *pa;
+//	cout << rra << endl;
+//	*pra = 20;
+//	cout << ra << endl;
+//}
