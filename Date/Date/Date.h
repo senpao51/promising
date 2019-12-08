3c32744f9b63e891b4b50453be30c8d935e839af
senpao51 <1773552201@qq.com>
2019-12-08 22:16:29 +0800

    完善Date类

--- Date/Date/Date.h
+++ Date/Date/Date.h
@@ -9,7 +9,7 @@ class Date
 public:
 	void DateExchangeStr(char*str);
 	long long TimeChangeTimestamp(const Date&d);
-	long long Date::TimeChangeTimestamp();
+	long long TimeChangeTimestamp();
 	Date(int year = 1970, int month = 1, int day = 1, int hour = 8, int minute = 0, int second = 0);
 	Date(const Date& d);
 	int GetYear()const;
--- Date/Date/test.cpp
+++ Date/Date/test.cpp
@@ -1,121 +1,120 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//#include "Date.h"
-//void main()
-//{
-//	Date dt1(2019,12,4);
-//	Date dt2 = dt1 + 7487;
-//	Date dt3(1970,12,4);
-//	Date dt4(1999, 12, 4);
-//	Date dt5 =dt2--;
-//	Date dt6;
-//
-//	//cin >>dt6 ;
-//	//cout << dt6 << endl;
-//	cout << (dt1 - dt3) << endl;
-//	//cout << dt2 << endl;
-//}
-#include<stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX 100000
-#define DataType char
-#define _DEAFAULT_QUEUE_SIZE 30
-typedef struct BinTreeNode
-{
-	DataType data;
-	struct BinTreeNode* left;
-	struct BinTreeNode* right;
-}BinTreeNode;
-
-typedef struct BinTree
-{
-	BinTreeNode* root;
-}BinTree;
-typedef struct Stack
-{
-	struct BinTreeNode*base;
-	int capacity;
-	int top;
-}Stack;
-BinTreeNode*_BinTreeCreateBy_VLR_LVR(char*vlr, char*lvr, int n)
-{
-	if (n == 0)
-		return 0;
-	int k = 0;
-	while (vlr[0] != lvr[k])
-		k++;
-	BinTreeNode*t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
-	t->data = vlr[0];
-	t->left = _BinTreeCreateBy_VLR_LVR(vlr + 1, lvr, k);
-	t->right = _BinTreeCreateBy_VLR_LVR(vlr + k + 1, lvr + k + 1, n - k - 1);
-	return t;
-}
-void BinTreeCreateBy_VLR_LVR(BinTree* t, char*vlr, char*lvr, int n)
-{
-	t->root = _BinTreeCreateBy_VLR_LVR(vlr, lvr, n);
-}
-void StackInit(Stack*st)
-{
-	st->capacity = MAX;
-	st->top = 0;
-	st->base = (struct BinTreeNode*)malloc(sizeof(struct BinTreeNode)*st->capacity);
-}
-void StackInsert(Stack*st, struct BinTreeNode*x)
-{
-	st->base[st->top++] = *x;
-}
-void StackDelete(Stack*st)
-{
-	st->top--;
-}
-struct BinTreeNode*GetStackTop(Stack*st)
-{
-	return &(st->base[st->top - 1]);
-}
-bool StackEmpty(Stack*st)
-{
-	return st->top == 0;
-}
-void PreOrder(struct BinTreeNode*root, int*returnSize, int*array)
-{
-	if (root == NULL)
-		return;
-	Stack st;
-	struct BinTreeNode*p;
-	StackInit(&st);
-	StackInsert(&st, root);
-	while (!StackEmpty(&st))
-	{
-		p = GetStackTop(&st);
-		GetStackTop(&st);
-		array[*returnSize] = p->data;
-		(*returnSize)++;
-		if (p->right != NULL)
-		{
-			StackInsert(&st, p->right);
-		}
-		if (p->left != NULL)
-		{
-			StackInsert(&st, p->left);
-		}
-	}
-}
-int* preorderTraversal(struct BinTreeNode* root, int* returnSize)
-{
-	*returnSize = 0;
-	int * array = (int*)malloc(sizeof(int)*MAX);
-	PreOrder(root, returnSize, array);
-	return array;
-}
-
+#include "Date.h"
 void main()
 {
-	char* vlr = "ABCDEFGH";
-	char *lvr = "CBEDFAGH";
-	int n = strlen(lvr);
-	BinTree bt;
-	BinTreeNode t;
-	BinTreeCreateBy_VLR_LVR(&bt, vlr, lvr, n);
-	int ret = 0;
-	preorderTraversal(&t, &ret);
+	Date dt1(2019,12,4);
+	Date dt2 = dt1 + 7487;
+	Date dt3(1970,12,4);
+	Date dt4(1999, 12, 4);
+	Date dt5 =dt2--;
+	Date dt6;
+	//cin >>dt6 ;
+	//cout << dt6 << endl;
+	cout << (dt1 - dt3) << endl;
+	//cout << dt2 << endl;
 }
+//#include<stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#define MAX 100000
+//#define DataType char
+//#define _DEAFAULT_QUEUE_SIZE 30
+//typedef struct BinTreeNode
+//{
+//	DataType data;
+//	struct BinTreeNode* left;
+//	struct BinTreeNode* right;
+//}BinTreeNode;
+//
+//typedef struct BinTree
+//{
+//	BinTreeNode* root;
+//}BinTree;
+//typedef struct Stack
+//{
+//	struct BinTreeNode*base;
+//	int capacity;
+//	int top;
+//}Stack;
+//BinTreeNode*_BinTreeCreateBy_VLR_LVR(char*vlr, char*lvr, int n)
+//{
+//	if (n == 0)
+//		return 0;
+//	int k = 0;
+//	while (vlr[0] != lvr[k])
+//		k++;
+//	BinTreeNode*t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+//	t->data = vlr[0];
+//	t->left = _BinTreeCreateBy_VLR_LVR(vlr + 1, lvr, k);
+//	t->right = _BinTreeCreateBy_VLR_LVR(vlr + k + 1, lvr + k + 1, n - k - 1);
+//	return t;
+//}
+//void BinTreeCreateBy_VLR_LVR(BinTree* t, char*vlr, char*lvr, int n)
+//{
+//	t->root = _BinTreeCreateBy_VLR_LVR(vlr, lvr, n);
+//}
+//void StackInit(Stack*st)
+//{
+//	st->capacity = MAX;
+//	st->top = 0;
+//	st->base = (struct BinTreeNode*)malloc(sizeof(struct BinTreeNode)*st->capacity);
+//}
+//void StackInsert(Stack*st, struct BinTreeNode*x)
+//{
+//	st->base[st->top++] = *x;
+//}
+//void StackDelete(Stack*st)
+//{
+//	st->top--;
+//}
+//struct BinTreeNode*GetStackTop(Stack*st)
+//{
+//	return &(st->base[st->top - 1]);
+//}
+//bool StackEmpty(Stack*st)
+//{
+//	return st->top == 0;
+//}
+//void PreOrder(struct BinTreeNode*root, int*returnSize, int*array)
+//{
+//	if (root == NULL)
+//		return;
+//	Stack st;
+//	struct BinTreeNode*p;
+//	StackInit(&st);
+//	StackInsert(&st, root);
+//	while (!StackEmpty(&st))
+//	{
+//		p = GetStackTop(&st);
+//		GetStackTop(&st);
+//		array[*returnSize] = p->data;
+//		(*returnSize)++;
+//		if (p->right != NULL)
+//		{
+//			StackInsert(&st, p->right);
+//		}
+//		if (p->left != NULL)
+//		{
+//			StackInsert(&st, p->left);
+//		}
+//	}
+//}
+//int* preorderTraversal(struct BinTreeNode* root, int* returnSize)
+//{
+//	*returnSize = 0;
+//	int * array = (int*)malloc(sizeof(int)*MAX);
+//	PreOrder(root, returnSize, array);
+//	return array;
+//}
+//
+//void main()
+//{
+//	char* vlr = "ABCDEFGH";
+//	char *lvr = "CBEDFAGH";
+//	int n = strlen(lvr);
+//	BinTree bt;
+//	BinTreeNode t;
+//	BinTreeCreateBy_VLR_LVR(&bt, vlr, lvr, n);
+//	int ret = 0;
+//	preorderTraversal(&t, &ret);
+//}
