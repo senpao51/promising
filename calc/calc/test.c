8cc73c3c32123cba495ac49930ac03727dc5ebe4
senpao51 <1773552201@qq.com>
2019-12-10 21:30:11 +0800

    二叉树，队列的改进

--- calc/calc/test.c
+++ calc/calc/test.c
@@ -1,38 +1,85 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <stdbool.h>
+#define DataType char
+typedef struct BinTreeNode
 {
-	int a = 0;
-	int b = 0;
-	char ch;
-	scanf("%d %c %d",&a,&ch,&b);
-	if (ch != '\\'&&b != 0)
+	DataType data;
+	struct BinTreeNode* leftchild;
+	struct BinTreeNode* rightchild;
+}BinTreeNode;
+typedef struct BinTree
+{
+	BinTreeNode* root;
+}BinTree;
+BinTreeNode* _BinTreeCreate_3(char*str, int*i)
+{
+	if (str[*i] == '#' || str[*i] == '\0')
+		return NULL;
+	else
 	{
-		switch (ch)
-		{
-		case '+':
-		{
-				printf("%d+%d=%d",a,b,a+b);
-				break;
-		}
-		case '-':
-		{
-				printf("%d-%d=%d", a, b, a - b);
-				break;
-		}
-		case '*':
-		{
-				printf("%d*%d=%d", a, b, a * b);
-				break;
-		}
-		case '/':
-		{
-				printf("%d/%d=%d", a, b, a / b);
-				break;
-		}
-		default:
-			break;
-		}
+		BinTreeNode*t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		t->data = str[*i];
+		(*i)++;
+		t->leftchild = _BinTreeCreate_3(str, i);
+		(*i)++;
+		t->rightchild = _BinTreeCreate_3(str, i);
+		return t;
 	}
 }
+void BinTreeCreate_3(BinTree*t, char*str, int*i)
+{
+	t->root = _BinTreeCreate_3(str, i);
+}
+
+
+
+//typedef struct Queue
+//{
+//
+//}Queue;
+
+void main()
+{
+	char *str = "ABC##DE##F##G#H##";
+	BinTree bt;
+	int i = 0;
+	BinTreeCreate_3(&bt, str, &i);
+}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	char ch;
+//	scanf("%d %c %d",&a,&ch,&b);
+//	if (ch != '\\'&&b != 0)
+//	{
+//		switch (ch)
+//		{
+//		case '+':
+//		{
+//				printf("%d+%d=%d",a,b,a+b);
+//				break;
+//		}
+//		case '-':
+//		{
+//				printf("%d-%d=%d", a, b, a - b);
+//				break;
+//		}
+//		case '*':
+//		{
+//				printf("%d*%d=%d", a, b, a * b);
+//				break;
+//		}
+//		case '/':
+//		{
+//				printf("%d/%d=%d", a, b, a / b);
+//				break;
+//		}
+//		default:
+//			break;
+//		}
+//	}
+//}
 
