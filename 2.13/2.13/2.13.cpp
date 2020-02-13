e600f2b2e0c7fc0c7bbb5720322e7892b34b4164
senpao51 <1773552201@qq.com>
2020-02-13 18:41:18 +0800

    Test

---
+++ 2.13/2.13/2.13.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+typedef struct s
+{
+	char ch;
+	int mark;
+}s;
+typedef struct Stack
+{
+	struct s*tmp;
+	int capacity;
+	int top;
+}Stack;
+void StackInit(Stack*st)
+{
+	st->capacity = 10000;
+	st->tmp = (s*)malloc(sizeof(s)*st->capacity);
+	st->tmp->ch = '\0';
+	st->tmp->mark = 0;
+	st->top = 0;
+}
+void StackInsert(Stack*st, char s)
+{
+	st->tmp->ch = s;
+	st->top++;
+}
+bool canConstruct(char * ransomNote, char * magazine)
+{
+	if (ransomNote == magazine)
+		return true;
+	Stack st;
+	StackInit(&st);
+	while (*magazine != '\0')
+	{
+		StackInsert(&st, *magazine);
+		magazine++;
+	}
+	while (*ransomNote != '\0')
+	{
+		for (int i = 0; <st.top; i++)
+		{
+
+		}
+	}
+}
+int main()
+{
+	canConstruct("fffbfg", "effjfggbffjdgbjjhhdegh");
+	return 0;
+}
