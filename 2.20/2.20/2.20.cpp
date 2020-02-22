703072e5cafd772e9ccf979b40a416f736ee4839
senpao51 <1773552201@qq.com>
2020-02-22 19:59:17 +0800

    vector类的模拟实现

--- 2.20/2.20/2.20.cpp
+++ 2.20/2.20/2.20.cpp
@@ -1,157 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <vector>
+#include <vld.h>
 using namespace std;
-namespace bit
-{
-	template<typename T>
-	class vector
-	{
-	public:
-		typedef T* iterator;
-	public:
-		vector() :start(nullptr), finish(nullptr), end_of_storage(nullptr)
-		{}
-		vector(size_t n, const T &value = T())
-			:start(nullptr), finish(nullptr), end_of_storage(nullptr)
-		{
-			reserve(n);
-			while (n-- != 0)
-				push_back(value);
-		}
-		template<class InputIterator>
-		vector(InputIterator first, InputIterator last)
-			:start(nullptr), finish(nullptr), end_of_storage(nullptr)
-		{
-			reserve(last - first);
-			while (first != last)
-			{
-				push_back(*first);
-				first++;
-			}
-		}
-		~vector()
-		{
-			delete[]start;
-			start = finish = end_of_storage = nullptr;
-		}
-	public:
-		size_t size()const
-		{
-			return finish - start;
-		}
-		size_t capacity()const
-		{
-			return end_of_storage - start;
-		}
-		bool empty()const
-		{
-			return finish == start;
-		}
-	public:
-		iterator begin()
-		{
-			return start;
-		}
-		iterator end()
-		{
-			return finish;
-		}
-	public:
-		void push_back(const T &x)
-		{
-			insert(end(), x);
-		}
-	public:
-		void swap(vector & v)
-		{
-			std::swap(start, v.start);
-			std::swap(finish, v.finish);
-			std::swap(end_of_storage, v.end_of_storage);
-		}
-		void reserve(size_t n)
-		{
-			if (n > capacity())
-			{
-				size_t oldsize = size();
-				T *new_start = new T[n];
-				if (start)
-				{
-					for (int i = 0; i<oldsize; ++i)
-					{
-						new_start[i] = start[i];
-					}
-				}
-
-				//释放原有空间
-				if (start)
-					delete[]start;
-
-				start = new_start;
-				finish = start + oldsize;
-				end_of_storage = start + n;
-			}
-		}
-		void resize(size_t n, const T& value = T())
-		{
-			if (n <= size())
-			{
-				finish = start + n;
-				return;
-			}
-
-			if (n > capacity())
-				reserve(n * 2);
-
-			iterator p = finish;
-			finish = start + n;
-			while (p != finish)
-			{
-				*p = value;
-				p++;
-			}
-		}
-	public:
-		iterator insert(iterator pos, const T &x)
-		{
-			if (finish >= end_of_storage)
-			{
-				size_t oldpos = pos - start;
-				size_t new_cpy = capacity() ? capacity() * 2 : 1;
-				reserve(new_cpy);
-
-				//重新定位迭代器
-				pos = start + oldpos;
-			}
-
-			iterator p = finish;
-			while (p != pos)
-			{
-				*p = *(p - 1);
-				--p;
-			}
-			*p = x;
-			finish++;
-			return pos;
-		}
-		iterator erase(iterator pos)
-		{
-			iterator p = pos;
-			while (p != finish - 1)
-			{
-				*p = *(p + 1);
-				p++;
-			}
-			finish--;
-			return pos;
-		}
-
-	private:
-		iterator start;
-		iterator finish;
-		iterator end_of_storage;
-	};
-};
 template<typename T>
 class Vector
 {
@@ -169,8 +20,28 @@ public:
 public:
 	Vector() :start(nullptr), finish(nullptr), last(nullptr)
 	{}
+	Vector(Vector<T>&t) :start(nullptr), finish(nullptr), last(nullptr)
+	{
+		Vector<T>tmp;
+		tmp.reserve(t.size());
+		for (int i = 0; i < t.size(); i++)
+		{
+			tmp.push_back(t[i]);
+		}
+		tmp.swap(*this);
+	}
+	Vector<T>& operator= (const Vector<T>&t)
+	{
+		if (*this != t)
+		{
+			Vector<T>tmp(t);
+			tmp.swap(*this);
+		}
+		return *this;
+	}
 	Vector(size_t num, const T&val = T()) :start(nullptr), finish(nullptr), last(nullptr)
 	{
+		//reserve(num);
 		for (int i = 0; i < num; i++)
 		{
 			push_back(val);
@@ -183,6 +54,12 @@ public:
 			push_back(*p1);
 			p1++;
 		}
+	}
+
+	~Vector()
+	{
+		delete[]start;
+		start = finish = last = nullptr;
 	}
 public:
 	size_t size()const
@@ -209,15 +86,14 @@ public:
 			T* new_start = new T[n];
 			if (start)
 			{
-				//memcpy(new_start,start,oldsize);
-				for (int i = 0; i <oldsize; i++)
+				for (int i = 0; i < oldsize; i++)
 				{
 					new_start[i] = start[i];
 				}
 				delete[] start;
 			}
 			start = new_start;
-			finish = start +oldsize;
+			finish = start + oldsize;
 			last = start + n;
 		}
 	}
@@ -229,7 +105,7 @@ public:
 		}
 		else
 		{
-			reserve(n+5);
+			reserve(n + 5);
 			iterator p = finish;
 			finish = start + n;
 			while (p != finish)
@@ -239,12 +115,13 @@ public:
 			}
 		}
 	}
-	void insert(iterator pos,const T&val)
+	void insert(iterator pos, const T&val)
 	{
 		if (size() >= capacity())
 		{
 			size_t oldpos = pos - start;
-			reserve(size()+5);
+			reserve(size() + 5);
+			//重新定位迭代器
 			pos = start + oldpos;
 		}
 		iterator p = finish;
@@ -258,17 +135,21 @@ public:
 	}
 	void insert(iterator pos, size_t n, const T&val)
 	{
+		int oldsize = pos - start;
+		insert(pos, val);
+		n--;
 		while (n--)
 		{
-			insert(pos, val);
+			insert(start + oldsize, val);
 		}
 	}
 	void insert(iterator pos, T*p1, T*p2)
 	{
+		int oldsize = pos - start;
+		insert(pos, *(--p2));
 		while (p1 != p2)
 		{
-			insert(pos,*p2);
-			p2--;
+			insert(start + oldsize, *(--p2));
 		}
 	}
 	void erase(iterator pos)
@@ -300,6 +181,17 @@ public:
 	{
 		erase(end());
 	}
+	void swap(Vector<T>&t)
+	{
+	std::swap(start,t.start);
+	std::swap(finish, t.finish);
+	std::swap(last, t.last);
+	}
+	T& operator[](int num)
+	{
+		return *(start + num);
+	}
+	
 private:
 	T*start;
 	T*finish;
@@ -307,65 +199,26 @@ private:
 };
 int main()
 {
-	Vector<int>v1;
-	for (auto &e : v1)
+	int arr[] = {1,2,3,4,5};
+	Vector<int>v1(5, 2);
+	Vector<int>v2(10,4);
+	v1.swap(v2);
+	Vector<int>v3(v2);
+	
+	/*for (int i = 0; i < v2.size(); i++)
+	{
+		cout << v2[i] << " ";
+	}
+	cout << endl;*/
+	for (auto&e : v3)
 	{
 		cout << e << " ";
 	}
 	cout << endl;
-	v1.reserve(10);
-	cout << v1.capacity() << endl;
-	for (auto &e : v1)
-	{
-		cout << e << " ";
-	}
-	v1.resize(20);
-	for (auto &e : v1)
+	Vector<int>v4 = v3;
+	for (auto&e : v4)
 	{
 		cout << e << " ";
 	}
 	cout << endl;
-	v1.insert(v1.begin()+2,8);
-	for (auto &e : v1)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-	v1.insert(v1.end(), 6);
-	for (auto &e : v1)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-	v1.erase(v1.begin() + 2);
-	v1.push_back(1);
-	v1.push_back(1);
-	v1.push_back(1);
-	v1.push_back(1);
-	v1.pop_back();
-
-	for (auto &e : v1)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-	cout << v1.capacity() << endl;
-	Vector <int>v2(10,5);
-	for (auto &e : v2)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-	int arr[] = { 1, 2, 3, 4, 5, 6 };
-	Vector<int>v5(arr,arr+6);
-	v5.insert(v5.begin(),arr,arr+6);
-	//v5.insert(v5.begin()+2,5,4567);
-	//v5.erase(v5.begin()+2, v5.end()-2);
-	for (auto &e : v5)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-	cout << v5.size() << endl;
-	return 0;
 }
