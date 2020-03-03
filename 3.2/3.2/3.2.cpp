731d14e5847fe7a77ddb8f83902e2701fc7a3bde
senpao51 <1773552201@qq.com>
2020-03-03 20:14:02 +0800

    Priority_Queue的模拟实现

--- 3.2/3.2/3.2.cpp
+++ 3.2/3.2/3.2.cpp
@@ -3,34 +3,111 @@
 #include <vector>
 #include <queue>
 #include <assert.h>
+#include <string>
 using namespace std;
-template<class T>
-struct less
+//namespace senpao
+//{
+//	template<class T>
+//	struct less
+//	{
+//		bool operator()(const T&left, const T&right)
+//		{
+//			return left < right;
+//		}
+//	};
+//
+//	template<class T>
+//	struct greater
+//	{
+//		bool operator()(const T&left, const T&right)
+//		{
+//			return left>right;
+//		}
+//	};
+//	template<class T, class Cont = vector<T>, typename Pred = less<T>>
+//	class Priority_Queue
+//	{
+//	public:
+//		typedef     T    value_type;
+//		typedef size_t   size_type;
+//		explicit Priority_Queue()
+//		{
+//			make_heap(c.begin(), c.end());
+//		}
+//		Priority_Queue(const value_type *first, const value_type *last) :c(first, last)
+//		{
+//			make_heap(c.begin(), c.end());
+//		}
+//		bool empty() const
+//		{
+//			return c.empty();
+//		}
+//		size_type size() const
+//		{
+//			return c.size();
+//		}
+//		value_type& top()
+//		{
+//			assert(!empty());
+//			return c.front();
+//		}
+//		const value_type& top() const
+//		{
+//			assert(!empty());
+//			return c.front();
+//		}
+//		void push(const value_type& x)
+//		{
+//			c.push_back(x);
+//			push_heap(c.begin(), c.end());
+//		}
+//		void pop()
+//		{
+//			c.pop_back();
+//			pop_heap(c.begin(), c.end());
+//		}
+//	protected:
+//		Cont c;
+//		Pred comp;
+//	};
+//};
+namespace senpao
 {
-	bool operator()(const T&left, const T&right)
+	template<class T>
+	struct less
 	{
-		return left < right;
-	}
-};
-template<class T>
-struct greator
-{
-	bool operator()(const T&left, const T&right)
+		bool operator()(const T&left, const T&right)
+		{
+			return left < right;
+		}
+	};
+
+	template<class T>
+	struct greater
 	{
-		return right>left;
-	}
-};
-template<class T,class Cont = vector<T>,class Com = less<T>>
-class Priority_Queue 
-{
+		bool operator()(const T&left, const T&right)
+		{
+			return left> right;
+		}
+	};
+	template<class T, class Cont = vector<T>, typename Pred = less<T>>
+	class Priority_Queue
+	{
 	public:
 		typedef     T    value_type;
 		typedef size_t   size_type;
-		explicit Priority_Queue() :c()
+		explicit Priority_Queue()
 		{
-			make_heap(c.begin(),c.end());
+
 		}
-		Priority_Queue(const value_type *first, const value_type *last);
+		Priority_Queue(const value_type *first, const value_type *last)
+		{
+			while (first != last)
+			{
+				c.push_back(*first++);
+				adjustup(c.size()-1);
+			}
+		}
 		bool empty() const
 		{
 			return c.empty();
@@ -52,27 +129,103 @@ class Priority_Queue
 		void push(const value_type& x)
 		{
 			c.push_back(x);
-			push_heap(c.begin(),c.end());
+			adjustup(c.size()-1);
 		}
 		void pop()
 		{
-			c.pop_back(x);
-			pop_heap(c.begin(),c.end());
+			std::swap(c.front(),c.back());
+			c.pop_back();
+			adjustdown(0);
+		}
+		void adjustdown(int parent)
+		{
+			int child = parent * 2 + 1;
+			while (child < c.size())
+			{
+				if ((child + 1 < c.size()) && comp(c[child],c[child+1]))
+					child++;
+				if (comp(c[parent], c[child]))
+				{
+					std::swap(c[parent], c[child]);
+					parent = child;
+					child = parent * 2 + 1;
+				}
+				else
+					break;
+			}
+		}
+		void adjustup(int child)
+		{
+			int parent = (child - 1) / 2;
+			while (child)
+			{
+				if (comp(c[parent], c[child]))
+				{
+					std::swap(c[parent], c[child]);
+					child = parent;
+					parent = (child - 1) / 2;
+				}
+				else
+					break;
+			}
 		}
 	protected:
 		Cont c;
-		Com comp;
+		Pred comp;
+	};
 };
-
+//int main()
+//{
+//	int arr[] = { 5, 9, 3, 400, 6, 7, 100 };
+//	senpao::Priority_Queue<int,vector<int>,senpao::greater<int>>Q(arr,arr+7);
+//	cout << Q.top() << endl;
+//	Q.pop();
+//	cout << Q.top() << endl;
+//	Q.pop();
+//	cout << Q.top() << endl;
+//	Q.pop();
+//	cout << Q.top() << endl;
+//	Q.pop(); 
+//	cout << Q.top() << endl;
+//	Q.pop();
+//	cout << Q.top() << endl;
+//	Q.pop();
+//	cout << Q.top() << endl;
+//	return 0;
+//}
 int main()
 {
-	Priority_Queue<int>Q;
-	Q.push(5);
-	Q.push(9);
-	Q.push(7);
-	Q.push(3);
-	Q.push(2);
-	Q.push(6);
-	cout << Q.top() << endl;
+	string s;
+	getline(cin, s);
+	int left = 0;
+	int right = s.size() - 1;
+	while (left<right)
+	{
+		std::swap(s[left++], s[right--]);
+	}
+	size_t  p = 0;
+	while (1)
+	{
+		size_t pos = s.find(' ', p);
+		if (string::npos != pos)
+		{
+			size_t q = pos + 1;
+			while (p != pos)
+			{
+				std::swap(s[p], s[pos - 1]);
+				p++;
+				pos--;
+			}
+			p = q;
+		}
+		else
+			break;
+	}
+	int max = s.size() - 1;
+	while (p != max)
+	{
+		std::swap(s[p++], s[max--]);
+	}
+	cout << s << endl;
 	return 0;
 }
