8d599fde6064f0bfbda1bb9a35e3a9b9cd5145e5
senpao51 <1773552201@qq.com>
2020-03-06 21:29:20 +0800

    Test

---
+++ 3.6/3.6/3.6.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+
+	return 0;
+}
+//bool ishuiwen(string s)
+//{
+//	string t = s;
+//	std::reverse(s.begin(), s.end());
+//	return s == t;
+//}
+//int main()
+//{
+//	string s;
+//	string t;
+//	cin >> s;
+//	cin >> t;
+//	int count = 0;
+//	string tmp = s;
+//	auto pos = s.begin();
+//	while (pos != s.end())
+//	{
+//		s.insert(pos, t.begin(), t.end());
+//		if (ishuiwen(s))
+//			count++;
+//		s = tmp;
+//		pos++;
+//	}
+//	cout << count << endl;
+//	return 0;
+//}
+//int main()
+//{
+//	return 0;
+//}
