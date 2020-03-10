cdb0a276d141b6c7fd4467a9fb1dcd3a4baf5917
senpao51 <1773552201@qq.com>
2020-03-10 20:36:37 +0800

    Test

---
+++ 3.10/3.10/3.10.cpp
@@ -0,0 +1,134 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+using namespace std;
+//class A
+//{
+//public:
+//	void fun()
+//	{
+//		cout << "A::fun()" << endl;
+//	}
+//	void fun(int a)
+//	{
+//		cout << "A::fun(a)" << endl;
+//	}
+//private:
+//	int a;
+//};
+//class B :public A
+//{
+//public:
+//	void fun()
+//	{
+//		cout << "B::fun()" << endl;
+//	}
+//private:
+//	int b;
+//};
+//int main()
+//{
+//	B b;
+//	b.fun(0);
+//	return 0;
+//}
+//class A
+//{
+//public:
+//	A()
+//	{
+//		cout << "A::A" << endl;
+//	}
+//	~A()
+//	{
+//		cout << "A::~A" << endl;
+//	}
+//	int a;
+//};
+//class B :public A
+//{
+//public:
+//	B()
+//	{
+//		cout << "B::B" << endl;
+//	}
+//	~B()
+//	{
+//		cout << "B::~B" << endl;
+//	}
+//	int b;
+//};
+//int main()
+//{
+//	B b;
+//	return 0;
+//}
+//class A
+//{
+//public:
+//	int a;
+//};
+//class B:public A
+//{
+//public:
+//	int a;
+//};
+//int main()
+//{
+//	B b;
+//	A a;
+//	a = b;
+//	A*pa = &b;
+//	A&qa = b;
+//	return 0;
+//}
+//int main()
+//{
+//	string s = "hello world";
+//	auto p = s.end();
+//	cout << *p << endl;
+//	return 0;
+//}
+//bool lexicographically(string s[],int n)
+//{
+//    for(int i =0;i<n;i++)
+//    {
+//        
+//    }
+//}
+//bool lengths(string s[], int n)
+//{
+//	for (int i = 0; i<n - 1; i++)
+//	{
+//		if (s[i].size()>s[i + 1].size())
+//			return false;
+//	}
+//	return true;
+//}
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	string s[100];
+//	for (int i = 0; i<n; i++)
+//	{
+//		cin >> s[i];
+//	}
+//	//    if(lexicographically(s,n)&&lengths(s,n))
+//	//    {
+//	//        cout<<"both"<<endl;
+//	//    }
+//	//    else if(lexicographically(s,n))
+//	//    {
+//	//        cout<<"lexicographically"<<endl;
+//	//    }
+//	if (lengths(s, n))
+//	{
+//		cout << "lengths" << endl;
+//	}
+//	else
+//	{
+//		cout << "none" << endl;
+//	}
+//	return 0;
+//}
