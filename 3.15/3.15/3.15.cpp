7f1a74a0a1f8c08c71d8b8082606878063c6ef70
senpao51 <1773552201@qq.com>
2020-03-15 21:53:24 +0800

    Test

---
+++ 3.15/3.15/3.15.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x = 3, y = 3;
+	switch (x % 2)
+	{
+	case 1:
+		switch (y)
+		{
+		case 0:cout << "first";
+		case 1:cout << "second"; break;
+		default: cout << "hello";
+		}
+	case 2:cout << "third";
+	}
+	return 0;
+}
