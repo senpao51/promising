10eff7e95c8453e04525d8632223d65d32ec889c
senpao51 <1773552201@qq.com>
2020-04-05 23:01:18 +0800

    Test

---
+++ 4.5/4.5/4.5.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	http://vyfsjklfbhj
+	cout << "hello" << endl;
+	goto http;
+	return 0; 
+}
+//int main()
+//{
+//	int n;
+//	while (cin >> n)
+//	{
+//		if (n == 0)
+//			break;
+//		long min = pow(5, n) - 4;
+//		long last = pow(4, n) + n - 4;
+//		cout << min << " " << last << endl;
+//	}
+//	return 0;
+//}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	while (getline(cin, s))
+//	{
+//		vector<char>v = { 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U' };
+//		for (int i = 0; i<s.size(); i++)
+//		{
+//			cout << v[s[i] - 65];
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <functional>
+//using namespace std;
+//int main()
+//{
+//	vector<int>v = {5,4,6,9,3,5,7,2,5,9,1,6};
+//	sort(v.begin(),v.end(),greater<int>());
+//	for (const auto&e : v)
+//		cout << e << " ";
+//	cout << endl;
+//	return 0;
+//}
